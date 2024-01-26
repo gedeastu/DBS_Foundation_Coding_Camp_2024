71b18d3ae624ca02f907290001c70afa2e2e9ceb
gedeastu <gede.astu@gmail.com>
2024-01-26 22:51:38 +0800

    form input event is done

--- Step01_Frontend_Web_Beginner/Form/FormInputEvent/FormInputEvent.js
+++ Step01_Frontend_Web_Beginner/Form/FormInputEvent/FormInputEvent.js
@@ -1,4 +1,61 @@
 document.addEventListener('DOMContentLoaded', function () {
     const inputMaxLengthOnLoad = document.getElementById('inputNama').maxLength;
     document.getElementById('sisaKarakter').innerText = inputMaxLengthOnLoad;
+
+    document.getElementById('inputNama').addEventListener('input', function () {
+        const jumlahKarakterDiketik = document.getElementById('inputNama').value.length;
+        const jumlahKarakterMaksimal = document.getElementById('inputNama').maxLength;
+        
+        console.log('jumlahKarakterDiketik: ', jumlahKarakterDiketik);
+        console.log('jumlahKarakterMaksimal: ', jumlahKarakterMaksimal);
+        const sisaKarakterUpdate = jumlahKarakterMaksimal - jumlahKarakterDiketik;
+        document.getElementById('sisaKarakter').innerText = sisaKarakterUpdate.toString();
+        
+        if (sisaKarakterUpdate === 0) {
+          document.getElementById('sisaKarakter').innerText = 'Karakter telah mencapa   i batas maksimal!';
+          const NotifikasiSisaKarakter = document.getElementById('notifikasiSisaKarakter').style.visibility = 'visible'
+        } else if (sisaKarakterUpdate <= 5) {
+          document.getElementById('notifikasiSisaKarakter').style.color = 'red';
+        } else {
+          document.getElementById('notifikasiSisaKarakter').style.color = 'black';
+        }
+    });
+
+    document.getElementById('inputNama').addEventListener('focus',function(){
+        const NotifikasiSisaKarakter = document.getElementById('notifikasiSisaKarakter').style.visibility = 'visible'
+    })
+
+    document.getElementById('inputNama').addEventListener('blur',function(){
+        const NotifikasiSisaKarakter = document.getElementById('notifikasiSisaKarakter').style.visibility = 'hidden'
+    })
+
+    document.getElementById('inputCaptcha').addEventListener('change',function(){
+        const inputCaptcha = document.getElementById('inputCaptcha').value;
+        const submitButtonStatus = document.getElementById('submitButton');
+
+        if (inputCaptcha == 'PRNU') {
+            submitButtonStatus.removeAttribute('disabled');
+        } else {
+            submitButtonStatus.removeAttribute('disabled','');
+        }
+    });
+
+    document.getElementById('formDataDiri').addEventListener('submit', function (event) {
+        const inputCaptcha = document.getElementById('inputCaptcha').value;
+        if (inputCaptcha === 'PRNU') {
+          alert('Selamat! Captcha Anda lolos :D');
+        } else {
+          alert('Captcha Anda belum tepat :(');
+          document.getElementById('submitButton').setAttribute('disabled', '');
+        }
+        event.preventDefault();
+    });
+
+    document.getElementById('inputCopy').addEventListener('copy', function () {
+        alert('Anda telah men-copy sesuatu...');
+    });
+     
+      document.getElementById('inputPaste').addEventListener('paste', function () {
+        alert('Anda telah mem-paste sebuah teks...');
+    });
 });
