370ad3c28f336cc759e07d4369872bf2ef98ac98
gedeastu <gede.astu@gmail.com>
2024-02-03 22:33:23 +0800

    return and show the list todo

--- Step01_Frontend_Web_Beginner/Todoapps/script.js
+++ Step01_Frontend_Web_Beginner/Todoapps/script.js
@@ -1,6 +1,6 @@
-
 const todos = [];
 const RENDER_EVENT = 'render-todo';
+
 document.addEventListener('DOMContentLoaded',function() {
     const submitForm = document.getElementById('form')
     submitForm.addEventListener('submit',function (event) {
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded',function() {
         addTodo();
     });
 });
+
 function addTodo() {
     const textTodo = document.getElementById('title').value;
     const timestamp = document.getElementById('date').value;
@@ -22,16 +23,36 @@ function addTodo() {
     document.addEventListener(RENDER_EVENT,function(){
         console.log(todos)
     })
+
 }
+
+function makeTodo(todoObject){
+    const textTitle = document.createElement('h2');
+    textTitle.innerText = todoObject.task;
+
+    const textTimestamp = document.createElement('p');
+    textTimestamp.innerText = todoObject.timestamp;
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('inner');
+    textContainer.append(textTitle,textTimestamp)
+
+    const container = document.createElement('div');
+    container.classList.add('item','shadow');
+    container.append(textContainer);
+    container.setAttribute('id',`todo-${todoObject.id}`);
+    return container;
+}
+
 function generateId() {
     return +new Date();
-  }
-   
-  function generateTodoObject(id, task, timestamp, isCompleted) {
+}
+
+function generateTodoObject(id, task, timestamp, isCompleted) {
     return {
       id,
       task,
       timestamp,
       isCompleted
     }
-  }
+}
